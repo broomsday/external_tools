a58b7b2e3780baf50e737d01044368703756f407
Aron Broom <broomsday@gmail.com>
2025-05-30 23:10:22 -0400

    Use newer condaforge names

--- install/install_proteinmpnn_step1.sh
+++ install/install_proteinmpnn_step1.sh
@@ -1,9 +1,9 @@
 PROTEINMPNNDIR="/ProteinMPNN"
 
 cd ${PROTEINMPNNDIR}
-wget -q -P . https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-bash ./Mambaforge-Linux-x86_64.sh -b -p ${PROTEINMPNNDIR}/conda
-rm Mambaforge-Linux-x86_64.sh
+wget -q -P . https://github.com/conda-forge/miniforge/releases/download/25.3.0-3/Miniforge3-25.3.0-3-Linux-x86_64.sh
+bash ./Miniforge3-25.3.0-3-Linux-x86_64.sh -b -p ${PROTEINMPNNDIR}/conda
+rm ./Miniforge3-25.3.0-3-Linux-x86_64.sh
 . "${PROTEINMPNNDIR}/conda/etc/profile.d/conda.sh"
 export PATH="${PROTEINMPNNDIR}/conda/condabin:${PATH}"
 conda create -p $PROTEINMPNNDIR/proteinmpnn-conda python=3.10 -y
